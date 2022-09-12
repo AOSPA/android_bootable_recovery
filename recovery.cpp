e0c2df215893ec2f5712b079d5db9d3601dc6922
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-12 21:07:19 +0530

    recovery: Support exfat formatted sdcards
    Quick and dirty hack, but works.
    
    Change-Id: Idfe0e58ddbb01d7ea4b2c62c68dbeba7a663549f

--- recovery.cpp
+++ recovery.cpp
@@ -375,6 +375,14 @@ static int do_sdcard_mount(RecoveryUI* ui)
                v->fs_type.c_str(),
                v->flags,
                v->fs_options.c_str());
+    if (rc) {
+        LOG(INFO) << "Failed to mount sdcard as vfat. Trying exfat";
+        rc = mount(SDCARD_BLK_0_PATH,
+                   v->mount_point.c_str(),
+                   "exfat",
+                   v->flags,
+                   v->fs_options.c_str());
+    }
   }
   else if (check_mmc_is_sdcard(MMC_1_TYPE_PATH) == 0) {
     LOG(INFO) << "Mounting sdcard on " << SDCARD_BLK_1_PATH;
@@ -383,6 +391,14 @@ static int do_sdcard_mount(RecoveryUI* ui)
                v->fs_type.c_str(),
                v->flags,
                v->fs_options.c_str());
+    if (rc) {
+        LOG(INFO) << "Failed to mount sdcard as vfat. Trying exfat";
+        rc = mount(SDCARD_BLK_1_PATH,
+                   v->mount_point.c_str(),
+                   "exfat",
+                   v->flags,
+                   v->fs_options.c_str());
+    }
   }
   else if (check_mmc_is_sdcard(SDEXPRESS_0_TYPE_PATH) == 0) {
     LOG(INFO) << "Mounting sdexpress on " << SDEXPRESS_BLK_0_PATH;
